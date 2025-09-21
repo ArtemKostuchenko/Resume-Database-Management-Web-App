6790578f35f1958dc3ec17cdffdf69cbf965b178
ArtemKostuchenko <ipzm241_kav@student.ztu.edu.ua>
2025-09-21 11:40:46 +0300

    Added display of submitted resumes on the main page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,30 @@
 
 namespace App\Controller;
 
+use App\Repository\ApplicationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(Request $request, ApplicationRepository $applicationRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        $page = $request->query->getInt('page', 1);
+        $limit= $request->query->getInt('limit', 10);
+        $offset = ($page - 1) * $limit;
+
+        $applications = $applicationRepository->findBy([], ['sent_at' => 'DESC'], $limit, $offset);
+        $total = $applicationRepository->count([]);
+
+        return $this->render('home/index.html.twig', [
+            'applications' => $applications,
+            'limit' => $limit,
+            'page' => $page,
+            'total' => $total,
+            'pages' => ceil($total / $limit),
+        ]);
     }
 }
